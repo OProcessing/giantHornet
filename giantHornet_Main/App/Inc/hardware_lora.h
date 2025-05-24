9f1882816df27c0e4d0dcee8a86d2c3e7fcdb2dd
Crem2y <31000852+Crem2y@users.noreply.github.com>
2025-05-24 19:46:09 +0900

    Update hardware_lora functions

--- giantHornet_Main/App/Inc/hardware_lora.h
+++ giantHornet_Main/App/Inc/hardware_lora.h
@@ -7,8 +7,8 @@
 #include "define.h"
 
 #define LORA_FREQ 434000000
-#define LORA_PACKET_LEN 16
-#define LORA_TIMEOUT 2000
+#define LORA_PACKET_LEN 32
+#define LORA_TIMEOUT 500
 
 USER_StatusTypeDef lora_init(SPI_HandleTypeDef* spi, uint8_t master_enable);
 USER_StatusTypeDef lora_send(void* data, uint8_t* len);
