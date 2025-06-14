4fa5acc38ba54b421b952a25d116c445116fc9cd
Crem2y <31000852+Crem2y@users.noreply.github.com>
2025-06-14 17:11:10 +0900

    Fix build error

--- giantHornet_FC/App/Inc/function_compute.h
+++ giantHornet_FC/App/Inc/function_compute.h
@@ -6,6 +6,7 @@
 // 제어 루프 초기화 (PID 등)
 void control_loop_init(void);
 void motor_SetPWM(int motor, int pwm);
+void motor_debug(int motor, int pwm);
 
 // 1kHz 제어 루프 (IMU→칼만→PID→모터)
 void control_loop(SPI_HandleTypeDef *SPIx, MPU9250_t *pMPU9250, Filtered_t *filtered);
