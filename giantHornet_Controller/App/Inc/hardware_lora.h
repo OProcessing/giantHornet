bdec9f7e727032eff4b195ad9628909d8933c0ad
Crem2y <31000852+Crem2y@users.noreply.github.com>
2025-07-26 21:20:57 +0900

    Fix controller build error
    This reverts commit 9f84689e6ffa2cf013ff9930fdfeb7db40483f21.

--- giantHornet_Controller/App/Inc/hardware_lora.h
+++ giantHornet_Controller/App/Inc/hardware_lora.h
@@ -1,12 +1,17 @@
 #ifndef INC_HARDWARE_LORA_H_
 #define INC_HARDWARE_LORA_H_
 
+#include "spi.h"
 #include "stdlib.h"
 #include "../../Drivers/SX1278/SX1278.h"
 #include "define.h"
 
-USER_StatusTypeDef lora_init(uint8_t master_enable);
-USER_StatusTypeDef lora_send(void* data, size_t* len);
-USER_StatusTypeDef lora_recv(void* data, size_t* len);
+#define LORA_FREQ 434000000
+#define LORA_PACKET_LEN 32
+#define LORA_TIMEOUT 500
+
+USER_StatusTypeDef lora_init(SPI_HandleTypeDef* spi, uint8_t master_enable);
+USER_StatusTypeDef lora_send(void* data, uint8_t* len);
+USER_StatusTypeDef lora_recv(void* data, uint8_t* len);
 
 #endif /* INC_HARDWARE_LORA_H_ */
